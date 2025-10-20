65f9cb4c22d10c466df0e044ba90aef8febef054
Isson-N <ilabessonov9@gmail.com>
2025-10-20 13:43:42 -0400

    Write FamiliesStat.py

--- data/FamiliesStat.py
+++ data/FamiliesStat.py
@@ -21,74 +21,66 @@ def add_family(df):
         real_families.append(name.split("_")[0].replace(">", ""))
     for name in df["query_name"].to_list():
         detected_families.append(name.split("_")[0])
-    new_df.insert(len(new_df.columns), 'real_family', real_families)
-    new_df.insert(len(new_df.columns), 'detected_family', detected_families)
+    new_df['real_family'] = real_families
+    new_df['detected_family'] = detected_families
     return new_df 
         
+def parse_model(args):
+	family, viral_df = args
+	dfm = viral_df[viral_df["real_family"] == family].copy()
 
+	dfm["positive"] = (dfm["real_family"] == dfm["detected_family"]).astype("int")
+
+	scores = dfm['full_score']
+	labels = dfm['positive']
+	fpr, tpr, thresholds = roc_curve(labels, scores)
+	roc_auc = auc(fpr, tpr)
+	if len(dfm[dfm["positive"] == 0]) == 0:
+		roc_auc = 1
+
+	plt.plot(fpr, tpr, color='blue', linewidth=0.5)
+
+	line = []
+	line.append(family)
+	line.append(roc_auc)
+
+	models_count = len(dfm)
+	line.append(models_count)
+
+	true_detect_count = len(dfm[dfm["positive"] == 1])
+	line.append(true_detect_count)
+
+	false_detect_count = len(dfm[dfm["positive"] == 0])
+	line.append(false_detect_count)
+ 
+	max_correct_full_score = np.max(dfm[dfm["positive"] == 1]["full_score"])
+	line.append(max_correct_full_score)
+
+	max_uncorrect_full_score = np.max(dfm[dfm["positive"] == 0]["full_score"])
+	line.append(max_uncorrect_full_score)
+
+	return line
+ 
+	
+	
+	
+
+# На этот раз обойдёмся без multiprocessing...
 # Применяем, а ещё добавляем инфу о том, вирусный или нет
 viral_df = parser("Viral_results.txt")
 viral_df = add_family(viral_df)
-print(viral_df["detected_family"].unique())
 
+results_df = pd.DataFrame(columns = ["Family", "AUC", "Models_count", "True_detect_count", "False_detect_count", "Max_correct_full_score", "Max_uncorrect_full_score"])
 
-
-def parse_model(args):
-	model, viral_df = args
-	print(model, end="\r")
-	dfm = combined_df[combined_df["query_name"] == model]
+fam_cnt = len(viral_df["real_family"].unique())
+k = 0
+for family in viral_df["real_family"].unique().tolist():
+	ln = parse_model((family, viral_df))
+	results_df.loc[len(results_df)] = ln
+	k+=1
+	print(f"\rВыполнил для семьи: {family}. Это {k} семья из {fam_cnt}")
  
-	scores = dfm['full_score']
-	labels = dfm['is_viral']
-	fpr, tpr, thresholds = roc_curve(labels, scores)
-	roc_auc = auc(fpr, tpr)
-	if len(dfm[dfm["is_viral"] == 0]) == 0:
-		roc_auc = 1
-	
-	line = []
-	line.append(model)
-	line.append(roc_auc)
-
-	try:
-		viral_max_full_score = np.max(dfm[dfm["is_viral"] == 1]["full_score"])
-	except:
-		viral_max_full_score = ""
-	line.append(viral_max_full_score)
-    
-	try:
-		nonviral_max_full_score = np.max(dfm[dfm["is_viral"] == 0]["full_score"])
-	except:
-		nonviral_max_full_score = ""
-	line.append(nonviral_max_full_score)
-    
-	try:
-		relation = viral_max_full_score/nonviral_max_full_score
-	except:
-		relation = ""
-	line.append(relation)
-    
-	try:
-		mean_only_viral = np.mean(dfm[(dfm["is_viral"] == 1) & (dfm["full_score"] >= nonviral_max_full_score)]["full_score"])
-	except:
-		mean_only_viral = ""
-	line.append(mean_only_viral) 
-
-	try:
-		viral_count = len(dfm[dfm["is_viral"] == 1])
-	except:
-		viral_count = ""
-	line.append(viral_count)
-
-	try:
-		nonviral_count = len(dfm[dfm["is_viral"] == 0])
-	except:
-		nonviral_count = ""
-	line.append(nonviral_count)
-
-	try:
-		only_viral = len(dfm[(dfm["is_viral"] == 1) & (dfm["full_score"] >= nonviral_max_full_score)])
-	except:
-		only_viral = ""
-	line.append(only_viral)
-	return line
-
+ 
+results_df.to_csv("Families_models.csv")
+plt.savefig("Families_ROC_curves.png")
+plt.show()
